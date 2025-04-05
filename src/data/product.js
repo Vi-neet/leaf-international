66e6fa51b69af713e7a63a734118a45f2d406062
Vineet Bhasin <bhasinvineet49@gmail.com>
2025-04-05 09:32:27 +0530

    rearranged pics

--- src/data/product.js
+++ src/data/product.js
@@ -1,25 +1,24 @@
 export const products = [
   {
     id: "1",
-    category: "Showers",
-    image: "/shower-1.jpg",
+    category: "Faucets",
+    image: "/faucet-2.jpg",
   },
   {
     id: "2",
     category: "Showers",
-    image: "/shower-2.jpg",
+    image: "/shower-1.jpg",
   },
   {
     id: "3",
-    category: "Concealed Parts",
-    image: "/cp-1.png",
+    category: "Showers",
+    image: "/shower-2.jpg",
   },
   {
     id: "4",
     category: "Handle Bath",
     image: "/hand-held-4.jpg",
   },
-  // Additional Plastic Taps (7 more)
   {
     id: "5",
     category: "Plastic Taps",
@@ -27,181 +26,193 @@ export const products = [
   },
   {
     id: "6",
-    category: "Plastic Taps",
-    image: "/Bib-Cock-with-Flange-white.webp",
+    category: "Concealed Parts",
+    image: "/cp-2.png",
   },
+
+  // Additional Plastic Taps (7 more)
+
   {
     id: "7",
     category: "Plastic Taps",
+
     image: "/long-pvc-taps-with-flage-blue.webp",
-  },
-  {
-    id: "8",
-    category: "Plastic Taps",
-    image: "/long-pvc-taps-with-flage-white.webp",
-  },
-  {
-    id: "9",
-    category: "Plastic Taps",
-    image: "/PTAC01_Bib-cock-with-flange-white.webp",
-  },
-  {
-    id: "10",
-    category: "Plastic Taps",
-    image: "/PTAC01B_Bib-Cock-with-Flange-blue.webp",
   },
   {
     id: "11",
     category: "Plastic Taps",
-    image: "/pvc-taps-with-flage-blue.webp",
+    image: "/PTAC01B_Bib-Cock-with-Flange-blue.webp",
   },
-  // Additional Handle Bath (3 more)
   {
     id: "12",
-    category: "Handle Bath",
-    image: "/hand-held-1.jpg",
+    category: "Plastic Taps",
+    image: "/pvc-taps-with-flage-blue.webp",
   },
+  {
+    id: "8",
+    category: "Plastic Taps",
+    image: "/Bib-Cock-with-Flange-white.webp",
+  },
+  {
+    id: "9",
+    category: "Plastic Taps",
+    image: "/long-pvc-taps-with-flage-white.webp",
+  },
+  {
+    id: "10",
+    category: "Plastic Taps",
+    image: "/PTAC01_Bib-cock-with-flange-white.webp",
+  },
+
+  // Additional Handle Bath (3 more)
   {
     id: "13",
     category: "Handle Bath",
-    image: "/hand-held-3.jpg",
+    image: "/hand-held-1.jpg",
   },
+
   {
-    id: "14",
+    id: "15",
     category: "Handle Bath",
     image: "/hand-held.jpg",
   },
   {
-    id: "15",
+    id: "16",
     category: "Handle Bath",
     image: "/hand-held-5.jpg",
   },
   {
-    id: "16",
+    id: "17",
     category: "Handle Bath",
     image: "/hand-held-6.jpg",
   },
+
   {
-    id: "17",
-    category: "Handle Bath",
-    image: "/hand-held-7.jpg",
-  },
-  {
-    id: "18",
+    id: "19",
     category: "Handle Bath",
     image: "/hand-held-8.jpg",
   },
   {
-    id: "19",
+    id: "20",
     category: "Handle Bath",
     image: "/hand-held-9.jpg",
   },
   {
-    id: "20",
+    id: "21",
     category: "Handle Bath",
     image: "/hand-held-10.jpg",
   },
+  {
+    id: "14",
+    category: "Handle Bath",
+    image: "/hand-held-3.jpg",
+  },
+  {
+    id: "18",
+    category: "Handle Bath",
+    image: "/hand-held-7.jpg",
+  },
   // Additional Tap (1)
   {
-    id: "21",
+    id: "22",
     category: "Faucets",
     image: "/Tap.jpg",
   },
   {
-    id: "22",
+    id: "23",
     category: "Plastic Taps",
     image: "/plastic-tap.jpg",
   },
-  { id: "23", category: "Plastic Taps", image: "/plastic-tap-1.jpg" },
   {
     id: "24",
-    category: "Showers",
-    image: "/shower-3.jpg",
+    category: "Plastic Taps",
+    image: "/plastic-tap-1.jpg",
   },
   {
     id: "25",
     category: "Showers",
-    image: "/shower-4.jpg",
+    image: "/shower-3.jpg",
   },
   {
     id: "26",
     category: "Showers",
-    image: "/shower-5.jpg",
+    image: "/shower-4.jpg",
   },
   {
     id: "27",
     category: "Showers",
-    image: "/shower-6.jpg",
+    image: "/shower-5.jpg",
   },
   {
     id: "28",
     category: "Showers",
-    image: "/shower-7.jpg",
+    image: "/shower-6.jpg",
   },
   {
     id: "29",
     category: "Showers",
-    image: "/shower-8.jpg",
+    image: "/shower-7.jpg",
   },
   {
     id: "30",
     category: "Showers",
-    image: "/shower-9.jpg",
+    image: "/shower-8.jpg",
   },
   {
     id: "31",
     category: "Showers",
-    image: "/shower-10.jpg",
+    image: "/shower-9.jpg",
   },
   {
     id: "32",
-    category: "Concealed Parts",
-    image: "/cp-2.png",
+    category: "Showers",
+    image: "/shower-10.jpg",
   },
+
   {
-    id: "33",
-    category: "Concealed Parts",
-    image: "/cp-3.png",
-  },
-  {
-    id: "34",
+    id: "35",
     category: "Concealed Parts",
     image: "/cp-4.png",
   },
   {
-    id: "35",
+    id: "36",
     category: "Concealed Parts",
     image: "/cp-5.png",
   },
   {
-    id: "36",
+    id: "37",
     category: "Concealed Parts",
     image: "/cp-6.png",
   },
   {
-    id: "37",
+    id: "38",
     category: "Concealed Parts",
     image: "/cp-7.png",
   },
   {
-    id: "38",
+    id: "39",
     category: "Concealed Parts",
     image: "/cp-8.jpg",
   },
   {
-    id: "39",
+    id: "40",
     category: "Concealed Parts",
     image: "/cp-9.jpg",
   },
   {
-    id: "40",
+    id: "41",
     category: "Concealed Parts",
     image: "/cp-10.jpg",
   },
   {
-    id: "41",
-    category: "Faucets",
-    image: "/faucet-2.jpg",
+    id: "33",
+    category: "Concealed Parts",
+    image: "/cp-1.png",
+  },
+  {
+    id: "34",
+    category: "Concealed Parts",
+    image: "/cp-3.png",
   },
   {
     id: "42",
